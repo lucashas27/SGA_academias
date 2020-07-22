ad46435687bc37d664641dbbd701472d9ffb4ece
LUCAS HENRIQUE ALVES SANTOS <lucashas27@gmail.com>
2020-07-22 16:25:57 -0300

    updated

--- routes.js
+++ routes.js
@@ -18,6 +18,9 @@ routes.get('/members', function(req, res) {
 
 routes.get('/instructors/:id', instructors.show)
 
+routes.get('/instructors/:id/edit', function(req, res){
+    return res.render('instructors/edit')
+})
 
 
 routes.get('/create', function(req, res){
