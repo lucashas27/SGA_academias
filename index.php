5a4f80866bda2a1863f34ef82046ed0c21b51615
LUCAS HENRIQUE ALVES SANTOS <lucashas27@gmail.com>
2021-03-20 16:54:43 -0300

    iniciando o setup

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema SGA</title>
+    <link rel="stylesheet" href="./public/css/index.css">
+</head>
+<body>
+<header> 
+
+<li>  
+<ol> Início </ol>
+<ol> Cadastrar Aluno </ol>
+<ol> Gerenciar Alunos </ol>
+<ol> Ver Instrutores </ol>
+
+</li>
+</header>
+    <p>teste</p>
+</body>
+</html>
